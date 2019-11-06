0369b9b4dc909b4089e0bae076e74c0eed1eb515
bucetabelen <bucetabmb@gmail.com>
2019-11-06 14:31:08 -0300

    agregue el paquete Jugador

--- src/main/java/fiuba/AlgoChess/Modelo/Jugador/Jugador.java
+++ src/main/java/fiuba/AlgoChess/Modelo/Jugador/Jugador.java
@@ -2,6 +2,16 @@ package fiuba.AlgoChess.Modelo.Jugador;
 
 public class Jugador {
 
-    private int puntos;
+    private int puntos = 20;
+    private int cantidadEntidades = 0; 
+
+    public bool colocarEntidad(Entidad unaEntidad){
+
+        if(this.puntos > 0){
+            retrun true;
+            cantidadEntidades++;
+        }else false;
+
+    }
 
 }
