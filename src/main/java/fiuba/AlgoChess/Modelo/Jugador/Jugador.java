f172f49eadf8154c0e2e2b27fd25845e717dba5f
Federico Pratto <fpratto@fi.uba.ar>
2019-11-10 17:33:46 -0300

    Agrego comentarios a la clase Jugador

--- src/main/java/fiuba/AlgoChess/Modelo/Jugador/Jugador.java
+++ src/main/java/fiuba/AlgoChess/Modelo/Jugador/Jugador.java
@@ -1,23 +1,38 @@
 package fiuba.AlgoChess.Modelo.Jugador;
 
 import fiuba.AlgoChess.Modelo.Unidad.Entidad;
-
 import java.util.Scanner;
 
 public class Jugador {
 
+    // Atributos.
+
     private String nombre;
     private int puntos;
-    private int cantidadEntidades = 0;
+    private int cantidadEntidades;
     Scanner consola = new Scanner(System.in);
 
+
+    // Metodos.
+
+    /*
+     * POST: Crea un nuevo Jugador al cual se le ha asignado un nombre y
+     * una cierta cantidad de puntos para jugar.
+     */
     public Jugador(String nombre){
 
         this.nombre = nombre;
         this.puntos = 20;
+        this.cantidadEntidades = 0;
     }
 
 
+    /*
+     * PRE:  El Jugador cuenta con puntos suficientes para colocar una nueva Entidad en el Tablero.
+     * POST: Se agrega una nueva Entidad al Tablero y al Jugador.
+     *
+     * NOTA: Revisar este metodo a ver si puedo mejorarlo.
+     */
     public boolean agregarEntidad(Entidad unaEntidad){
 
         if(this.puntos >= unaEntidad.getCosto()){ //precioentidad
@@ -29,15 +44,22 @@ public class Jugador {
         }else{
             return false;
         }
-
     }
 
-    //funciona como controlador de estado de jugador.
+
+    /*
+     * POST: Indica si el Jugador aun tiene fichas para continuar jugando.
+     */
     public boolean sigueJugando() {
 
         return(this.cantidadEntidades > 0);
     }
 
+
+    /*
+     * PRE:  -...
+     * POST: -...
+     */
     public int[] elegirCasillero(){
 
         int[] array = new int[2];
@@ -51,7 +73,12 @@ public class Jugador {
         return array;
     }
 
+
+    /*
+     * POST: Devuelve los puntos del jugador.
+     */
     public void setPuntos(int puntos) {
+
         this.puntos = puntos;
     }
 }
