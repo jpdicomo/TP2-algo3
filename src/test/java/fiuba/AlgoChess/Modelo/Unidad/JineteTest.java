88ad60beabc58099913a850642b9cfc36b5dba15
Federico Pratto <fpratto@fi.uba.ar>
2019-11-13 19:40:15 -0300

    Funcionan los test de Jinete

--- src/test/java/fiuba/AlgoChess/Modelo/Unidad/JineteTest.java
+++ src/test/java/fiuba/AlgoChess/Modelo/Unidad/JineteTest.java
@@ -1,68 +1,59 @@
 package fiuba.AlgoChess.Modelo.Unidad;
 
-
-import fiuba.AlgoChess.Modelo.Tablero.Casillero.Casillero;
+import org.junit.Assert;
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.mockito.Mockito.*;
+import fiuba.AlgoChess.Modelo.Errores.MismaUnidadException;
+import fiuba.AlgoChess.Modelo.Errores.MismoBandoException;
+import fiuba.AlgoChess.Modelo.Jugador.Jugador;
+
 
 public class JineteTest {
-    @Test
-    public void test00JineteApenasCreadoTiene100Vida(){
-        Jinete jinete = new Jinete(mock(Casillero.class));
 
-        assertEquals(100,jinete.getVida());
-    }
+	
+	@Test
+	public void test00JineteApenasCreadoTiene100DeVida() {
+		
+		Jugador jose = new Jugador("Jose");
+		Jinete jinete = new Jinete(jose.getBando());
 
-    @Test
-    public void test01JineteAliadoAtacaAJineteEnemigoYLeRestaCorrectamenteLaVida(){
-        Jinete jinete1 = new Jinete(mock(Casillero.class));
-        jinete1.setBandoAliado();
-        Jinete jinete2 = new Jinete(mock(Casillero.class));
-        jinete2.setBandoEnemigo();
+		Assert.assertEquals(100, jinete.getVida());
+	}
 
-        jinete1.atacarA(jinete2);
-        assertEquals(100-5,jinete2.getVida());
-    }
-    @Test
-    public void test02JineteAliadoAtacaAJineteAliadoTiraError(){
-        Jinete jinete1 = new Jinete(mock(Casillero.class));
-        jinete1.setBandoAliado();
-        Jinete jinete2 = new Jinete(mock(Casillero.class));
-        jinete2.setBandoAliado();
-        assertFalse(jinete1.atacarA(jinete2));
+	
+	@Test
+	public void test01JineteAtacaAJineteEnemigoYLeRestaCorrectamenteLaVida() {
+		
+		Jugador jose = new Jugador("Jose");
+		Jinete jinete1 = new Jinete(jose.getBando());
+		
+		Jugador juan = new Jugador("Juan");
+		Jinete jinete2 = new Jinete(juan.getBando());
 
-    }
-    @Test
-    public void test03JineteEnemigoAtacaAJineteAliadoYLeRestaCorrectamenteLaVida(){
-        Jinete jinete1 = new Jinete(mock(Casillero.class));
-        jinete1.setBandoEnemigo();
-        Jinete jinete2 = new Jinete(mock(Casillero.class));
-        jinete2.setBandoAliado();
-        jinete1.atacarA(jinete2);
-        assertEquals(100-5,jinete2.getVida());
-    }
-    @Test
-    public void test04JineteEnemigoAtacaAJineteEnemigoTiraError(){
-        Jinete jinete1 = new Jinete(mock(Casillero.class));
-        jinete1.setBandoEnemigo();
-        Jinete jinete2 = new Jinete(mock(Casillero.class));
-        jinete2.setBandoEnemigo();
-        assertFalse(jinete1.atacarA(jinete2));
-    }
-    @Test
-    public void test05UnJineteAliadoNoPuedeAtacarseASiMismo(){
-        Jinete jinete = new Jinete(mock(Casillero.class));
-        jinete.setBandoAliado();
-        assertFalse(jinete.atacarA(jinete));
-    }
-    @Test
-    public void test06UnJineteEnemigoNoPuedeAtacarseASiMismo(){
-        Jinete jinete = new Jinete(mock(Casillero.class));
-        jinete.setBandoEnemigo();
-        assertFalse(jinete.atacarA(jinete));
-    }
+		jinete1.realizarAccionSobre(jinete2);
+		
+		Assert.assertEquals(95, jinete2.getVida());
+	}
+
+	
+	@Test (expected = MismoBandoException.class)
+	public void test02JineteAtacaAJineteAliadoTiraError() {
+		
+		Jugador jose = new Jugador("Jose");
+		
+		Jinete jinete1 = new Jinete(jose.getBando());
+		Jinete jinete2 = new Jinete(jose.getBando());
+
+		jinete1.realizarAccionSobre(jinete2);
+	}
+	
+
+	@Test (expected = MismaUnidadException.class)
+	public void test03UnJineteNoPuedeAtacarseASiMismo() {
+		
+		Jugador jose = new Jugador("Jose");
+		
+		Jinete jinete1 = new Jinete(jose.getBando());
+		jinete1.realizarAccionSobre(jinete1);
+	}
 }
-
