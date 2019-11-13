04c307e39b9832d9f1829c35f9d759eecb24497f
bucetabelen <bucetabmb@gmail.com>
2019-11-13 16:32:21 -0300

    Se edita Test00

--- src/test/java/fiuba/AlgoChess/Modelo/Tablero/TableroTest.java
+++ src/test/java/fiuba/AlgoChess/Modelo/Tablero/TableroTest.java
@@ -1,6 +1,7 @@
 package fiuba.AlgoChess.Modelo.Tablero;
 
 
+import fiuba.AlgoChess.Modelo.Unidad.Aliada;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -11,15 +12,17 @@ public class TableroTest {
 
     @Test
     public void Test00CreacionTablero(){
-/*
-        HashMap<Posicion, String > tablero = new HashMap<>();
+
+        HashMap<Posicion, Casillero > tablero = new HashMap<>();
         Posicion pos = new Posicion (1,1);
-        tablero.put(pos,"hola");
+   
+        tablero.put(pos,new Casillero(1, 1, new Aliada()));
+        Casillero c = tablero.get(pos);
         Posicion posB = new Posicion (1,1);
 
-        Assert.assertEquals("hola", tablero.get(pos));
-*/
+        Assert.assertEquals(1, c.getBando());
 
+/*
         Tablero tablero = new Tablero();
 
         //ver que hay en el casillero 0,0;
@@ -32,7 +35,7 @@ public class TableroTest {
             }
 
         }
-
+*/
 
     }
 
