62d8324e89ba7947553e5d5e7a6b793e63852c72
Federico Pratto <fpratto@fi.uba.ar>
2019-11-20 02:15:20 -0300

    Test del Jinete completos

--- src/test/java/fiuba/AlgoChess/Modelo/Unidad/CuranderoTest.java
+++ src/test/java/fiuba/AlgoChess/Modelo/Unidad/CuranderoTest.java
@@ -3,8 +3,12 @@ package fiuba.AlgoChess.Modelo.Unidad;
 import org.junit.Assert;
 import org.junit.Test;
 
+import fiuba.AlgoChess.Modelo.Errores.CatapultaNoPuedeSerCuradaException;
+import fiuba.AlgoChess.Modelo.Errores.DistanciaInvalidaException;
 import fiuba.AlgoChess.Modelo.Errores.DistintoBandoException;
+import fiuba.AlgoChess.Modelo.Errores.MismaUnidadException;
 import fiuba.AlgoChess.Modelo.Jugador.Bando;
+import fiuba.AlgoChess.Modelo.Tablero.Casillero.Casillero;
 
 
 public class CuranderoTest {
@@ -14,6 +18,7 @@ public class CuranderoTest {
     public void test00CuranderoRecienCreadoTiene75DeVida(){
     	
     	Bando bandoJose = new Bando();
+    	
         Curandero curandero = new Curandero(bandoJose);
 
         Assert.assertEquals(75, curandero.getVida());
@@ -21,43 +26,106 @@ public class CuranderoTest {
     
     
     @Test
-    public void test01CuranderoCuraACuranderoAliadoNoDaniadoSumaSusPuntosDeVidaMaximos(){
+    public void test01CuranderoCuraAAliadoYLeSumaSusPuntosDeVidaMaximos(){
         
     	Bando bandoJose = new Bando();
     	
     	Curandero curandero1 = new Curandero(bandoJose);
-        Curandero curandero2 = new Curandero(bandoJose);
+    	Curandero curandero2 = new Curandero(bandoJose);
+    	Casillero casillero1 = new Casillero(0, 0, bandoJose);
+    	Casillero casillero2 = new Casillero(0, 1, bandoJose);    	
 
-        curandero1.interactuarCon(curandero2);
-
-        Assert.assertEquals(75 + 15,curandero2.getVida());
-    }
-    
-    
-    @Test
-    public void test02CuranderoCuraACuranderoAliadoDaniadoSumaSusPuntosDeVida(){
-    	
-    	Bando bandoJose = new Bando();
-    	
-    	Curandero curandero1 = new Curandero(bandoJose);
-        Curandero curandero2 = new Curandero(bandoJose);
+        casillero1.agregarNuevaUnidad(curandero1);
+        casillero2.agregarNuevaUnidad(curandero2);
         
-        curandero2.recibirDanio(15);
-        curandero1.interactuarCon(curandero2);
+        curandero1.interactuarCon(casillero2);
 
-        Assert.assertEquals(75,curandero2.getVida());
+        Assert.assertEquals(75 + 15, curandero2.getVida());
     }
     
+    
+	@Test
+	public void test02CuranderoCuraACuranderoAliadoDaniadoSumaSusPuntosDeVida() {
+
+		Bando bandoJose = new Bando();
+
+		Curandero curandero1 = new Curandero(bandoJose);
+		Curandero curandero2 = new Curandero(bandoJose);
+		Casillero casillero1 = new Casillero(0, 0, bandoJose);
+		Casillero casillero2 = new Casillero(0, 1, bandoJose);
+
+		casillero1.agregarNuevaUnidad(curandero1);
+		casillero2.agregarNuevaUnidad(curandero2);
+
+		curandero2.recibirDanio(15);
+		curandero1.interactuarCon(casillero2);
+
+		Assert.assertEquals(75, curandero2.getVida());
+	}
+	
+	@Test (expected = DistanciaInvalidaException.class)
+	public void test03CuranderoIntentaCurarAAliadoMuyLejosYTiraError() {
+
+		Bando bandoJose = new Bando();
+
+		Curandero curandero1 = new Curandero(bandoJose);
+		Curandero curandero2 = new Curandero(bandoJose);
+		Casillero casillero1 = new Casillero(0, 0, bandoJose);
+		Casillero casillero2 = new Casillero(0, 3, bandoJose);
+
+		casillero1.agregarNuevaUnidad(curandero1);
+		casillero2.agregarNuevaUnidad(curandero2);
+
+		curandero2.recibirDanio(15);
+		curandero1.interactuarCon(casillero2);
+	}
+	
     
     @Test (expected = DistintoBandoException.class)
-    public void test03CuranderoCuraACuranderoEnemigoTiraError(){
+    public void test04CuranderoCuraACuranderoEnemigoTiraError(){
     	
-    	Bando bandoJose = new Bando();
-		Curandero curandero1 = new Curandero(bandoJose);
-
+		Bando bandoJose = new Bando();
 		Bando bandoJuan = new Bando();
-		Curandero curandero2 = new Curandero(bandoJuan);
 
-		curandero1.interactuarCon(curandero2);
+		Curandero curandero1 = new Curandero(bandoJose);
+		Curandero curandero2 = new Curandero(bandoJuan);
+		Casillero casillero1 = new Casillero(0, 0, bandoJose);
+		Casillero casillero2 = new Casillero(0, 1, bandoJuan);
+
+		casillero1.agregarNuevaUnidad(curandero1);
+		casillero2.agregarNuevaUnidad(curandero2);
+
+		curandero1.interactuarCon(casillero2);
+    }
+    
+    
+    @Test (expected = CatapultaNoPuedeSerCuradaException.class)
+    public void test05CuranderoQuiereCurarCatapultaYTiraError(){
+    	
+		Bando bandoJose = new Bando();
+
+		Curandero curandero = new Curandero(bandoJose);
+		Catapulta catapulta = new Catapulta(bandoJose);
+		Casillero casillero1 = new Casillero(0, 0, bandoJose);
+		Casillero casillero2 = new Casillero(0, 1, bandoJose);
+
+		casillero1.agregarNuevaUnidad(curandero);
+		casillero2.agregarNuevaUnidad(catapulta);
+
+		curandero.interactuarCon(casillero2);
+    }
+    
+    
+    @Test (expected = MismaUnidadException.class)
+    public void test06CuranderoSeCuraASiMismoYTiraError(){
+    	
+		Bando bandoJose = new Bando();
+
+		Curandero curandero = new Curandero(bandoJose);
+		Casillero casillero = new Casillero(0, 0, bandoJose);
+
+		casillero.agregarNuevaUnidad(curandero);
+
+		curandero.interactuarCon(casillero);
     }
 }
--- src/test/java/fiuba/AlgoChess/Modelo/Unidad/SoldadoTest.java
+++ src/test/java/fiuba/AlgoChess/Modelo/Unidad/SoldadoTest.java
@@ -3,12 +3,18 @@ package fiuba.AlgoChess.Modelo.Unidad;
 import org.junit.Assert;
 import org.junit.Test;
 
+import fiuba.AlgoChess.Modelo.Errores.DistanciaInvalidaException;
 import fiuba.AlgoChess.Modelo.Errores.MismaUnidadException;
 import fiuba.AlgoChess.Modelo.Errores.MismoBandoException;
 import fiuba.AlgoChess.Modelo.Jugador.Bando;
+import fiuba.AlgoChess.Modelo.Tablero.Casillero.Casillero;
 
 
 public class SoldadoTest {
+	
+	/*
+	 * NOTA: Los test de batallon estan en TableroTest.
+	 */
 
 	
 	@Test
@@ -25,37 +31,68 @@ public class SoldadoTest {
 	public void test01SoldadoAliadoAtacaASoldadoEnemigoYLeRestaCorrectamenteLaVida() {
 		
 		Bando bandoJose = new Bando();
-		Soldado soldado1 = new Soldado(bandoJose);
+    	Bando bandoJuan = new Bando();
 
-		Bando bandoJuan = new Bando();
-		Soldado soldado2 = new Soldado(bandoJuan);
-
-		soldado1.interactuarCon(soldado2);
+    	Soldado soldado1 = new Soldado(bandoJose);
+    	Soldado soldado2 = new Soldado(bandoJuan);
+		Casillero casillero1 = new Casillero(0, 0, bandoJose);
+		Casillero casillero2 = new Casillero(0, 1, bandoJuan);
+		
+		casillero1.agregarNuevaUnidad(soldado1);
+		casillero2.agregarNuevaUnidad(soldado2);
+		
+		soldado1.interactuarCon(casillero2);
 		
 		Assert.assertEquals(90, soldado2.getVida());
+	}
+	
+	
+	@Test (expected = DistanciaInvalidaException.class)
+	public void test02SoldadoAtacaASoldadoEnemigoMuyLejosYTiraError() {
+		
+		Bando bandoJose = new Bando();
+    	Bando bandoJuan = new Bando();
+
+    	Soldado soldado1 = new Soldado(bandoJose);
+    	Soldado soldado2 = new Soldado(bandoJuan);
+		Casillero casillero1 = new Casillero(0, 0, bandoJose);
+		Casillero casillero2 = new Casillero(0, 3, bandoJuan);
+		
+		casillero1.agregarNuevaUnidad(soldado1);
+		casillero2.agregarNuevaUnidad(soldado2);
+		
+		soldado1.interactuarCon(casillero2);
 	}
 
 	
 	@Test (expected = MismoBandoException.class)
-	public void test02SoldadoAtacaASoldadoAliadoTiraError() {
+	public void test03SoldadoAtacaASoldadoAliadoTiraError() {
 		
 		Bando bandoJose = new Bando();
-		
-		Soldado soldado1 = new Soldado(bandoJose);
-		Soldado soldado2 = new Soldado(bandoJose);
 
-		soldado1.interactuarCon(soldado2);
+    	Soldado soldado1 = new Soldado(bandoJose);
+    	Soldado soldado2 = new Soldado(bandoJose);
+		Casillero casillero1 = new Casillero(0, 0, bandoJose);
+		Casillero casillero2 = new Casillero(0, 1, bandoJose);
+		
+		casillero1.agregarNuevaUnidad(soldado1);
+		casillero2.agregarNuevaUnidad(soldado2);
+		
+		soldado1.interactuarCon(casillero2);
 	}
 	
 
 	@Test (expected = MismaUnidadException.class)
-	public void test03UnSoldadoNoPuedeAtacarseASiMismo() {
+	public void test04UnSoldadoSeAtacaASiMismoYTiraError() {
 
 		Bando bandoJose = new Bando();
+
+    	Soldado soldado = new Soldado(bandoJose);
+		Casillero casillero = new Casillero(0, 0, bandoJose);
 		
-		Soldado soldado1 = new Soldado(bandoJose);
+		casillero.agregarNuevaUnidad(soldado);
 		
-		soldado1.interactuarCon(soldado1);
+		soldado.interactuarCon(casillero);
 	}
 
 }
