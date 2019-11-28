6c4f6757b6d55990cf24963d539df8c96ba3a322
jpdicomo <jdicomo@fi.uba.ar>
2019-11-27 21:44:22 -0300

    Incluidos los cambios de la rama InterfazGráficaFede

--- src/main/java/fiuba/AlgoChess/Vista/Juego/Main.java
+++ src/main/java/fiuba/AlgoChess/Vista/Juego/Main.java
@@ -3,14 +3,15 @@ package fiuba.AlgoChess.Vista.Juego;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.StackPane;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 import javafx.scene.canvas.*;
 
@@ -19,8 +20,8 @@ import java.io.FileNotFoundException;
 
 public class Main extends Application {
 
-	Stage ventana;
-	Scene sceneMenu, scene2;
+	private Stage escenario;
+	private Scene sceneMenu, scene2;
 
 
 	public Main() throws FileNotFoundException {
@@ -33,35 +34,55 @@ public class Main extends Application {
 
 	@Override
 	public void start(Stage stage) throws Exception {
-		ventana = stage;
+		this.escenario = stage;
+		this.escenario.setTitle("AlgoChess");
 
-		stage.setTitle("AlgoChess");
 
-		//Elementos 1
-		Label label1 = new Label("Binevenido a AlgoChess!");
-		Button botonJugar = new Button("Jugar");
-		botonJugar.setOnAction(e -> ventana.setScene(scene2));
-
-		//Primer layout
-		VBox layout1 = new VBox(20);
-		layout1.getChildren().addAll(label1 ,botonJugar);
-		sceneMenu = new Scene(layout1,600,600);
-
-		//Boton 2
-		Button botonVolver = new Button("Volver");
-		botonVolver.setOnAction(e -> ventana.setScene(sceneMenu));
+//
+//		//Boton 2
+//		Button botonVolver = new Button("Volver");
+//		botonVolver.setOnAction(e -> ventana.setScene(sceneMenu));
 
 		//Imagen tile
 		Image tileSprite = new Image(new FileInputStream("texturas/tileSprite.png"));
+//
+//		//Segundo Layout
+//		Pane layout2 = new Pane();
+//		layout2.getChildren().add(botonVolver);
+//		setCasilleros(layout2,tileSprite);
+//		scene2 = new Scene(layout2,600,600);
 
-		//Segundo Layout
-		Pane layout2 = new Pane();
-		layout2.getChildren().add(botonVolver);
-		setCasilleros(layout2,tileSprite);
-		scene2 = new Scene(layout2,600,600);
-
-		ventana.setScene(sceneMenu);
+		escenario.setScene(this.pantallaInicial());
 		stage.show();
+	}
+	private Scene pantallaInicial() throws FileNotFoundException {
+
+		Label labelTitulo = new Label("AlgoChess");
+		labelTitulo.setFont(Font.font("Verdana", 48));
+		labelTitulo.setTextFill(Color.rgb(255, 255, 255));
+
+		Button botonNuevaPartida = new Button("Nueva Partida");
+		botonNuevaPartida.setFont(Font.font("Verdana", 16));
+
+		Button botonSalir = new Button("Salir");
+		botonSalir.setFont(Font.font("Verdana", 16));
+
+		VBox botones = new VBox(botonNuevaPartida, botonSalir);
+		botones.setAlignment(Pos.BOTTOM_CENTER);
+		botones.setSpacing(15);
+
+		VBox contenedorPrincipal = new VBox(labelTitulo, botones);
+		contenedorPrincipal.setSpacing(200);
+		contenedorPrincipal.setAlignment(Pos.CENTER);
+
+		Background fondo = new Background(new BackgroundImage(new Image(new FileInputStream("texturas/fondo.png")),
+				BackgroundRepeat.REPEAT,
+				BackgroundRepeat.REPEAT,
+				BackgroundPosition.CENTER,
+				new BackgroundSize(800, 600, false, false, false, false)));
+		contenedorPrincipal.setBackground(fondo);
+
+		return new Scene(contenedorPrincipal, 800, 600);
 	}
 
 	public void setCasilleros(Pane layout,Image img){
