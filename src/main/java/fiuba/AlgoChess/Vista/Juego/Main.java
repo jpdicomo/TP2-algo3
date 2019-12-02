214125df030bcbcdb07934d79e9b5b10c7c67b03
jpdicomo <jdicomo@fi.uba.ar>
2019-12-01 22:16:52 -0300

    Añadida la posibilidad de que ambos jugadores puedan poner unidades

--- src/main/java/fiuba/AlgoChess/Vista/Juego/Main.java
+++ src/main/java/fiuba/AlgoChess/Vista/Juego/Main.java
@@ -200,20 +200,36 @@ public class Main extends Application {
 	
 	public Scene escenaColocarUnidades(int numeroJugador) {
 		
-		Jugador jugador = this.getJugador(numeroJugador);
+		Jugador jugador1 = this.getJugador(1);
 		
-		// Titulo
-		Label labelTitulo = new Label(jugador.getNombre() + " - Coloca tus unidades");
-		labelTitulo.setFont(Font.font("Times New Roman", 28));
+		// Titulo1
+		Label labelTitulo1 = new Label(jugador1.getNombre() + " - Coloca tus unidades");
+		labelTitulo1.setFont(Font.font("Times New Roman", 28));
 
 		// Caja de unidades
-		VistaUnidadSeleccionada unidadSeleccionada = new VistaUnidadSeleccionada();
-		CajaDeUnidadesVertical cajaUnidades = new CajaDeUnidadesVertical(this, numeroJugador, unidadSeleccionada);
-		
-		
+		VistaUnidadSeleccionada unidadSeleccionada1 = new VistaUnidadSeleccionada();
+		CajaDeUnidadesVertical cajaUnidades1 = new CajaDeUnidadesVertical(this, 1, unidadSeleccionada1);
+//---------------------------------------------------------------------------------------
+		Jugador jugador2 = this.getJugador(1+1);
+
+		// Titulo2
+		Label labelTitulo2 = new Label(jugador2.getNombre() + " - Coloca tus unidades");
+		labelTitulo2.setFont(Font.font("Times New Roman", 28));
+
+		// Caja de unidades
+		VistaUnidadSeleccionada unidadSeleccionada2 = new VistaUnidadSeleccionada();
+		CajaDeUnidadesVertical cajaUnidades2 = new CajaDeUnidadesVertical(this, 1+1, unidadSeleccionada2);
+
+
 		// Tablero
 		VistaTablero tablero = new VistaTablero(this, this.tablero);
-		tablero.compartamientoColocarUnidades(unidadSeleccionada);
+		//tablero.compartamientoColocarUnidades(unidadSeleccionada1);
+		Button colocarUnidades1 = new Button("Colocar unidades rojas");
+		colocarUnidades1.setOnAction(e -> tablero.compartamientoColocarUnidades(unidadSeleccionada1));
+		Button colocarUnidades2 = new Button("Colocar unidades verdes");
+		colocarUnidades2.setOnAction(e -> tablero.compartamientoColocarUnidades(unidadSeleccionada2));
+		//tablero.compartamientoColocarUnidades(unidadSeleccionada2);
+//---------------------------------------------------------------------------------------
 		
 		/*
 		 * Te quedaste aca, hay que ponerle comportamiento a los botones de las
@@ -231,14 +247,25 @@ public class Main extends Application {
 		
 		
 		// Creo las cajas.
-		VBox cajaDeUnidades = new VBox(cajaUnidades, unidadSeleccionada);
-		cajaDeUnidades.setSpacing(20);
-		
-		HBox unidadesYTablero = new HBox(cajaDeUnidades, tablero);
+		VBox cajaDeUnidades1 = new VBox(cajaUnidades1, unidadSeleccionada1);
+		cajaDeUnidades1.setSpacing(20);
+
+		VBox cajaDeUnidades2 = new VBox(cajaUnidades2, unidadSeleccionada2);
+		cajaDeUnidades2.setSpacing(20);
+
+		HBox unidadesYTablero = new HBox(cajaDeUnidades1, tablero,cajaDeUnidades2);
 		unidadesYTablero.setSpacing(20);
 		unidadesYTablero.setAlignment(Pos.CENTER);
-		
-		VBox contenedorSecundario = new VBox(labelTitulo, unidadesYTablero);
+
+		HBox labels = new HBox(labelTitulo1,labelTitulo2);
+		labels.setSpacing(640);
+		labels.setAlignment(Pos.CENTER);
+
+		HBox botones = new HBox(colocarUnidades1,colocarUnidades2);
+		botones.setSpacing(640);
+		botones.setAlignment(Pos.CENTER);
+
+		VBox contenedorSecundario = new VBox(labels, unidadesYTablero,botones);
 //		contenedorSecundario.setMaxWidth(515);
 //		contenedorSecundario.setMinHeight(450);
 		contenedorSecundario.setBackground(new CreadorDeFondos().crearFondo("./recursos/compra/fondo.png", 800, 600));
