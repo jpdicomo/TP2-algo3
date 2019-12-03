37681315094a1ae2cfc777977953d7f60405b6d1
bucetabelen <bucetabmb@gmail.com>
2019-12-03 20:56:37 -0300

    agrego test CURANDERO y modifico Casillero para el correcto funcionamiento de las pruebas del curandero

--- src/test/java/fiuba/AlgoChess/Modelo/Unidad/CuranderoTest.java
+++ src/test/java/fiuba/AlgoChess/Modelo/Unidad/CuranderoTest.java
@@ -13,37 +13,37 @@ import fiuba.AlgoChess.Modelo.Tablero.Casillero.Casillero;
 
 public class CuranderoTest {
 
-	
-    @Test
-    public void test00CuranderoRecienCreadoTiene75DeVida(){
-    	
-    	Bando bandoJose = new Bando();
-    	
-        Curandero curandero = new Curandero(bandoJose);
 
-        Assert.assertEquals(75, (int) curandero.getVida());
-    }
-    
-    
-    @Test
-    public void test01CuranderoCuraAAliadoYLeSumaSusPuntosDeVidaMaximos(){
-        
-    	Bando bandoJose = new Bando();
-    	
-    	Curandero curandero1 = new Curandero(bandoJose);
-    	Curandero curandero2 = new Curandero(bandoJose);
-    	Casillero casillero1 = new Casillero(0, 0, bandoJose);
-    	Casillero casillero2 = new Casillero(0, 1, bandoJose);    	
+	@Test
+	public void test00CuranderoRecienCreadoTiene75DeVida(){
 
-        casillero1.agregarNuevaUnidad(curandero1);
-        casillero2.agregarNuevaUnidad(curandero2);
-        
-        curandero1.interactuarCon(casillero2);
+		Bando bandoJose = new Bando();
 
-        Assert.assertEquals(75 + 15, (int) curandero2.getVida());
-    }
-    
-    
+		Curandero curandero = new Curandero(bandoJose);
+
+		Assert.assertEquals(75, (int)curandero.getVida());
+	}
+
+
+	@Test
+	public void test01CuranderoCuraAAliadoNoDaniadoYLeSumaSusPuntosDeVidaMaximos(){
+
+		Bando bandoJose = new Bando();
+
+		Curandero curandero1 = new Curandero(bandoJose);
+		Curandero curandero2 = new Curandero(bandoJose);
+		Casillero casillero1 = new Casillero(0, 0, bandoJose);
+		Casillero casillero2 = new Casillero(0, 1, bandoJose);
+
+		casillero1.agregarNuevaUnidad(curandero1);
+		casillero2.agregarNuevaUnidad(curandero2);
+
+		curandero1.interactuarCon(casillero2);
+
+		Assert.assertEquals(75 + 15,  (int)curandero2.getVida());
+	}
+
+
 	@Test
 	public void test02CuranderoCuraACuranderoAliadoDaniadoSumaSusPuntosDeVida() {
 
@@ -60,11 +60,80 @@ public class CuranderoTest {
 		curandero2.recibirDanio(15);
 		curandero1.interactuarCon(casillero2);
 
-		Assert.assertEquals(75, (int) curandero2.getVida());
+		Assert.assertEquals(75,  (int)curandero2.getVida());
 	}
-	
+
+
+	@Test
+	public void test03CuranderoCuraASoldadoAliadoDaniadoSumaSusPuntosDeVida() {
+
+		Bando bandoJose = new Bando();
+
+		Curandero curandero = new Curandero(bandoJose);
+		Soldado soldado = new Soldado(bandoJose);
+		Casillero casillero1 = new Casillero(0, 0, bandoJose);
+		Casillero casillero2 = new Casillero(0, 1, bandoJose);
+
+		casillero1.agregarNuevaUnidad(curandero);
+		casillero2.agregarNuevaUnidad(soldado);
+
+		soldado.recibirDanio(20);
+		curandero.interactuarCon(casillero2);
+
+		Assert.assertEquals(95,  (int)soldado.getVida());
+	}
+
+
+	@Test
+	public void test04CuranderoCuraASoldadoAliadoDaniadoQueEstaEnCasilleroEnemigoYLoCuraIgual() {
+
+		Bando bandoJose = new Bando();
+		Bando bandoJuan = new Bando();
+
+		Curandero curandero = new Curandero(bandoJose);
+		Curandero curandero2 = new Curandero(bandoJose);
+		Soldado soldado = new Soldado(bandoJose);
+		Casillero casillero1 = new Casillero(0, 0, bandoJose);
+		Casillero casillero2 = new Casillero(0, 1, bandoJuan);
+
+		casillero1.agregarNuevaUnidad(curandero);
+		casillero2.agregarNuevaUnidad(curandero2);
+		casillero2.recibirDanio(75);
+		casillero2.quitarUnidadMuerta();
+		casillero2.agregarUnidad(soldado);
+
+
+		soldado.recibirDanio(40);
+
+		curandero.interactuarCon(casillero2);
+
+		Assert.assertEquals(75,  (int)soldado.getVida());
+	}
+
+	@Test
+	public void test05CuranderoCuraAJineteAliadoDaniadoSumaSusPuntosDeVida() {
+
+		Bando bandoJose = new Bando();
+
+		Curandero curandero = new Curandero(bandoJose);
+		Jinete jinete = new Jinete(bandoJose);
+		Casillero casillero1 = new Casillero(0, 0, bandoJose);
+		Casillero casillero2 = new Casillero(0, 1, bandoJose);
+
+		casillero1.agregarNuevaUnidad(curandero);
+		casillero2.agregarNuevaUnidad(jinete);
+
+		jinete.recibirDanio(20);
+		curandero.interactuarCon(casillero2);
+
+		Assert.assertEquals(95,  (int)jinete.getVida());
+	}
+
+
+
+
 	@Test (expected = DistanciaInvalidaException.class)
-	public void test03CuranderoIntentaCurarAAliadoMuyLejosYTiraError() {
+	public void test06CuranderoIntentaCurarAAliadoMuyLejosYTiraError() {
 
 		Bando bandoJose = new Bando();
 
@@ -79,11 +148,11 @@ public class CuranderoTest {
 		curandero2.recibirDanio(15);
 		curandero1.interactuarCon(casillero2);
 	}
-	
-    
-    @Test (expected = DistintoBandoException.class)
-    public void test04CuranderoCuraACuranderoEnemigoTiraError(){
-    	
+
+
+	@Test (expected = DistintoBandoException.class)
+	public void test07CuranderoCuraACuranderoEnemigoTiraError(){
+
 		Bando bandoJose = new Bando();
 		Bando bandoJuan = new Bando();
 
@@ -96,12 +165,12 @@ public class CuranderoTest {
 		casillero2.agregarNuevaUnidad(curandero2);
 
 		curandero1.interactuarCon(casillero2);
-    }
-    
-    
-    @Test (expected = CatapultaNoPuedeSerCuradaException.class)
-    public void test05CuranderoQuiereCurarCatapultaYTiraError(){
-    	
+	}
+
+
+	@Test (expected = CatapultaNoPuedeSerCuradaException.class)
+	public void test08CuranderoQuiereCurarCatapultaYTiraError(){
+
 		Bando bandoJose = new Bando();
 
 		Curandero curandero = new Curandero(bandoJose);
@@ -113,12 +182,12 @@ public class CuranderoTest {
 		casillero2.agregarNuevaUnidad(catapulta);
 
 		curandero.interactuarCon(casillero2);
-    }
-    
-    
-    @Test (expected = MismaUnidadException.class)
-    public void test06CuranderoSeCuraASiMismoYTiraError(){
-    	
+	}
+
+
+	@Test (expected = MismaUnidadException.class)
+	public void test09CuranderoSeCuraASiMismoYTiraError(){
+
 		Bando bandoJose = new Bando();
 
 		Curandero curandero = new Curandero(bandoJose);
@@ -127,5 +196,5 @@ public class CuranderoTest {
 		casillero.agregarNuevaUnidad(curandero);
 
 		curandero.interactuarCon(casillero);
-    }
+	}
 }
